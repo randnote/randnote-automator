82b60829d70f243b8e4d4896f680e761070610df
danielromeo <danielromeo99@gmail.com>
2022-05-08 03:53:31 +0200

    able to make a random transaction with the blockchain api

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from transactions.main import main as TransactionsMain # importing the transactions processes
 
+''' ROMEO'S KEYS 
+PRIVATE:  d56f2bd3eb7c5612fbc04c96342102327c7c9ae44abf01ed5316604747d6f111
+PUBLIC:   047b1ca607797acfdd5d27173275484cf97ac0bc434a5fee5bd2a1e086cfe5ed42fc43220068120875e64713971f185fa538108b5992f494228f6865112c953453
+'''
 
 def main():
     TransactionsMain()
