c56b7342aff854ea20ad79073966c5f580d901da
danielromeo <danielromeo99@gmail.com>
2022-06-13 19:36:12 +0200

    working on the csv keygen js files

--- keygen.js
+++ keygen.js
@@ -5,29 +5,41 @@ const fs = require('fs');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const key = ec.genKeyPair();
-const publicKey = key.getPublic("hex");
-const privateKey = key.getPrivate("hex");
-
-// console.log("Private Key: ", privateKey);
-// console.log("Public key: ", publicKey);
-
 
 const csvWriter = createCsvWriter({
     path: 'out.csv',
     header: [
-      {id: 'private', title: 'Privatekey'},
-      {id: 'public', title: 'Publickey'},
+      {id: 'public', title: 'Public'},
+      {id: 'private', title: 'Private'},
      
     ]
-  });
+});
   
-  const data = [
-    {
-      name: 'John',
-      surname: 'dfhdsfhsdfh'
-    }
-  ];
+const data = [
+{
+    public:     key.getPublic("hex"),
+    private:    key.getPrivate("hex")
+}
+];
+
+// const  append = async()=>{
+//     for (let i = 0; i < 100; i++) {
+//         data.push({
+//             public:     await key.getPublic("hex"),
+//             private:   await key.getPrivate("hex")
+//         })
+//        await  console.log( await key.getPublic("hex"))
+        
+//     }
+// }
+
+// append()
+
+const checkAndWrite = () =>{
+    //
+    // row_count = sum(1 for row in fileObject)  # fileObject is your csv.reader
+}
   
-  csvWriter
+csvWriter
     .writeRecords(data)
-    .then(()=> console.log('The CSV file was written successfully and the keys have been added'));
+    .then(checkAndWrite());
