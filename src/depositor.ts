14198fcd5d38e17f1fb172a73d1b1fcd56a9f9bd
danielromeo <danielromeo99@gmail.com>
2022-11-07 16:49:24 +0200

    added addiditonal info in the app

--- src/depositor.ts
+++ src/depositor.ts
@@ -1,4 +1,6 @@
 /* 
     Use the users array and get the user with the lowest balance, then deposit into their account, simple
 
+    To deposit, the user needs to have a card... - When creating these users, also create a card for them...
+    -Then write a route in the backend that returns a users card
 */
